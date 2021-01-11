795e6e775df2d402a0ce5913cb88bf377ffde814
HEssaidi <EssaidiHajar50@gmail.com>
2021-01-11 10:41:42 +0100

    second commit remove localhost

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://localhost:27017/e-learning-app', { useNewUrlParser: true })
+    .connect('mongodb://0.0.0.0:27017/e-learning-app', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
