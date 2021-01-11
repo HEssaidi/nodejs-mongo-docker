4645045d2bd7fba11509ca21edb69f781d42bba3
HEssaidi <EssaidiHajar50@gmail.com>
2021-01-11 13:48:47 +0100

    seventh commit add docker-compose for mongo

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://0.0.0.0:27017/e-learning-app', { useNewUrlParser: true })
+    .connect('mongodb://mongo:27017/e-learning-app', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
